76c247d3a127741415ed5f890eef0ce761dd179e
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-14 22:12:03 -0400

    Update part5_scatter.py

--- src/part5_scatter.py
+++ src/part5_scatter.py
@@ -8,8 +8,19 @@ PART 5: SCATTER PLOTS
 # 1. Using lmplot, create a scatter plot where the x-axis is the prediction for felony and the y-axis the is prediction for a nonfelony, and hue this by whether the current charge is a felony. 
 # 
 # In a print statement, answer the following question: What can you say about the group of dots on the right side of the plot?
+def scatterplot_felony_vs_nonfelony(pred_universe):
+    sns.lmplot(data=pred_universe, x='prediction_felony', y='prediction_nonfelony', hue='has_felony_charge', fit_reg=False)
+    plt.savefig('./data/part5_plots/scatter_felony_vs_nonfelony.png', bbox_inches='tight')
 
-
+    # Interpretation
+    print("What can you say about the group of dots on the right side of the plot?")
+  
 # 2. Create a scatterplot where the x-axis is prediction for felony rearrest and the y-axis is whether someone was actually rearrested.
 # 
 # In a print statement, answer the following question: Would you say based off of this plot if the model is calibrated or not?
+def scatterplot_felony_vs_actual_rearrest(pred_universe):
+    sns.lmplot(data=pred_universe, x='prediction_felony', y='rearrest_felony', fit_reg=False)
+    plt.savefig('./data/part5_plots/scatter_felony_vs_actual_rearrest.png', bbox_inches='tight')
+  
+    # Interpretation
+    print("What does this plot suggest about how well the model is calibrated?")
